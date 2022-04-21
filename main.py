ee8bdfa6f0900fce32b0ebc9d55f734c40896ea5
joey57 <njorogejoey5@gmail.com>
2022-04-21 11:29:51 +0300

    take one player input

--- main.py
+++ main.py
@@ -9,13 +9,40 @@
 # function to check tie
 # function to flip player
 
+
 board = ["-","-","-",
          "-","-","-",
          "-","-","-"]
+currentPlayer = "x"
+winner = None
+gameRunning = True         
 
-def display_board():
-  print(board[0] + " | " + board[1] + " | " + board[2])   
+
+def printBoard(board):
+  print(board[0] + " | " + board[1] + " | " + board[2]) 
+  print("--------------")  
   print(board[3] + " | " + board[4] + " | " + board[5])  
+  print("--------------")
   print(board[6] + " | " + board[7] + " | " + board[8]) 
-   
-display_board()        
+  print("---------------")
+
+# take player input
+def playerInput(board):
+  inp = int(input("Enter a number 1-9: "))
+  if inp >= 1 and inp <= 9 and board[inp-1] =="-":
+    board[inp-1] = currentPlayer
+  else:
+    print("Ooops player is already in that spot!")  
+
+
+
+
+# check for win or tie again
+while gameRunning:
+  printBoard(board)
+  playerInput(board)
+
+
+
+  
+      
