7a0bd3a48afd980c847d74282115b329cf2390f3
joey57 <njorogejoey5@gmail.com>
2022-04-21 11:45:36 +0300

    check row function

--- main.py
+++ main.py
@@ -45,7 +45,22 @@ def checkHorizontle(board):
     return True
   elif board[6] == board[7] == board[8] and board[6] != "-":
     winner = board[6]
+    return True
+
+# check for rows
+def checkRow(board):
+  global winner
+  if board[0] == board[3] == board[6] and board[0] != "-":
+    winner = board[0]
+    return True
+  elif board[1] == board[4] == board[7] and board[1] != "-":
+    winner = board[1]
     return True  
+  elif board[2] == board[5] == board[8] and board[2] != "-":
+    winner = board[2]
+    return True  
+  
+ 
 
 
 
